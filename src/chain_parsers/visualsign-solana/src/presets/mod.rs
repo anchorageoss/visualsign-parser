67681fe23ffaa34f9e436b43db6b30b05e398f8c
Kyle Kamimoto <kyle.kamimoto@anchorlabs.com>
2026-05-01 15:31:31 +0000

    add jupiter perps program

--- src/chain_parsers/visualsign-solana/src/presets/mod.rs
+++ src/chain_parsers/visualsign-solana/src/presets/mod.rs
@@ -3,6 +3,7 @@ pub mod compute_budget;
 pub mod dflow_aggregator;
 pub mod jupiter_borrow;
 pub mod jupiter_earn;
+pub mod jupiter_perps;
 pub mod jupiter_swap;
 pub mod kamino_borrow;
 pub mod kamino_farms;
